597b18008887c1de73e40e94250800239a3ff6e5
aidawhale <whaleaida@gmail.com>
2026-02-07 22:30:40 +0100

    Initial Commit

---
+++ whale-gym/whale_gymApp.swift
@@ -0,0 +1,17 @@
+//
+//  whale_gymApp.swift
+//  whale-gym
+//
+//  Created by aida on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct whale_gymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
