16bcfb7a52a484fa4d957b6aff9efc79793c8edf
aidawhale <whaleaida@gmail.com>
2026-02-15 17:24:55 +0100

    add basic WelcomePage, TrainingCardList and TabView following tutorials

---
+++ whale-gym/TrainingCard.swift
@@ -0,0 +1,18 @@
+//
+//  TrainingCard.swift
+//  whale-gym
+//
+//  Created by aida on 15/2/26.
+//
+
+import SwiftUI
+
+struct TrainingCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrainingCard()
+}
---
+++ whale-gym/TrainingListPage.swift
@@ -0,0 +1,18 @@
+//
+//  TrainingListPage.swift
+//  whale-gym
+//
+//  Created by aida on 15/2/26.
+//
+
+import SwiftUI
+
+struct TrainingListPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrainingListPage()
+}
